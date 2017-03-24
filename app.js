b1b14526645a68bd91b9ee22e8762ecfd4df6421
ghigbie <gehigbie@gmail.com>
2017-03-24 23:42:13 +0000

    fix typo is app.listen

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    seedDB      = require("./seeds.js");
+const express     = require("express"),
+      bodyParser  = require("body-parser"),
+      mongoose    = require("mongoose"),
+      app         = express();
 
-mongoose.connect("mongodb://localhost/yelp_camp_v3");
+// self created packages 
+const Campground = require("./models/campground"),
+      seedDB     = require("./seeds.js");
+
+
+mongoose.connect("mongodb://localhost/yelp_camp_v4");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 seedDB();
-    
-app.get("/", function(req, res){
-    res.render("landing");
-});
 
-app.get("/search", function(req, res){
-    res.render("search");
+
+app.get("/", function(req, res){
+    res.render("landing"); 
 });
 
 
@@ -32,9 +32,6 @@ app.get("/campgrounds", function(req, res){
     });
 });
 
-app.get("/search", function(req, res){
-    res.render("search");
-});
 
 //CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){ //this is the REST convention - should be the same url
@@ -54,47 +51,30 @@ app.post("/campgrounds", function(req, res){ //this is the REST convention - sho
     });
 });
 
-
-//NEW - show form to create new campground
+// NEW -show form to create a new campground
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
 });
 
-//this needs to be positioned AFTER "/campgrounds/new"
-//SHOW - shows more info about one campgroud
+
+// SHOW - shows more info aboout one campground - this needs to be positioned AFTER "/campgrounds/new"
 app.get("/campgrounds/:id", function(req, res){
-    //find the campground with provided ID
+    // find the campground with the provided ID
     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
-            console.log("THERE WAS AN ERROR - CAMPGROUNDS/:ID");
+            console.log("THERE WAS A PROBLEM - CAMPGROUNDS/:ID");
             console.log(err);
-        }else {
+        }else{
             console.log(foundCampground);
-            res.render("show", {campground: foundCampground} );
+            res.render("show", {campground: foundCampground});
         }
     });
 });
 
-    
-   // res.render("show");
-    //find the campground with provided ID
-    // Campground.findById(req.params.id, function(err, foundCampground){
-    //     if(err){
-    //         console.log("THERE WAS AN ERROR - CAMPGROUNDS/:ID");
-    //         console.log(err);
-    //     }else{
-    //         //render show template with this campground
-    //         console.log("RES.RENDER CALLED");
-    //         res.render("show", {campground: foundCampground});   
-    //     }
-    // });
-// 
-
 app.get("*", function(req, res){
-   res.render("notFound"); 
+    res.render("notFound");
 });
 
-
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The server has started. Yay!!! Time to celebrate : )");
+    console.log("The server is running"); 
 });
