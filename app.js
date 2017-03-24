c3f05ab4d40aea1db619cffb0ddf1fa4189d8c67
ghigbie <gehigbie@gmail.com>
2017-03-24 22:59:22 +0000

    add /campgrounds route and search route

--- app.js
+++ app.js
@@ -4,4 +4,19 @@ const express     = require("express"),
       app         = express();
 
 // self created packages 
-const Campground = require("./models/campground");
+const Campground = require("./models/campground"),
+      seedDB     = require("./seeds.js");
+
+
+mongoose.connect("mongodb://localhost/yelp_camp_v4");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+seedDB();
+
+
+app.get("/", function(req, res){
+    res.render("landing"); 
+});
+
+
+
