5a507b4a9d830c757a210cb571e54226a2e6291a
ghigbie <gehigbie@gmail.com>
2017-03-24 22:08:48 +0000

    add model and schema to campground.js

--- models/campground.js
+++ models/campground.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+let campgroundSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    comments = [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: comment
+            
+        
+         }
+    ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
