785ed454ccbc387ed82ca4d6e59f78b92187a7a7
ghigbie <gehigbie@gmail.com>
2017-03-24 22:01:10 +0000

    add require statment vars

---
+++ app.js
@@ -0,0 +1,7 @@
+const express     = require("express"),
+      bodyParser  = require("body-parser"),
+      mongoose    = require("mongoose"),
+      app         = express();
+
+// self created packages 
+const Campground  = require(./models/campground);
